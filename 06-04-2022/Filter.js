4f050300d32a23a4124ea7bbfacea3d4d832a234
MiguelPaes <100474687+MiguelPaes@users.noreply.github.com>
2022-04-06 20:31:09 -0300

    Aula De Filter

---
+++ 06-04-2022/Filter.js
@@ -0,0 +1,18 @@
+// const numeros = [12, 34, 76, 89, 36, 120, 43]
+// console.log(listarNumeros50(numeros))
+
+// function listarNumeros50 (array){
+    //     let superiores50 = []
+    //     for (let i = 0; i < array.length; i ++){
+        //         if(array[i] > 50){
+            //            superiores50.push(array[i])
+            
+            //         }
+            //     }
+            // return superiores50
+            // }
+            
+            
+            const numeros = [12, 34, 76, 89, 36, 120, 43]
+
+            const filtrarNumeros50 = numeros.filter( numero => numero > 50)
